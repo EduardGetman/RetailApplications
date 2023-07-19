0c56210d338f9492a3eba805bb7db8d9a39056eb
Эдуард Гетман <getman.edik2016@yandex.ru>
2023-07-19 21:07:25 +0300

    Добавлены сервисы реализующие CRUD для сущностей Product и Warehouse

---
+++ Services/WarehouseService/Application/Services/ProductService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WarehouseService.Data.Domain;
+using WarehouseService.Infrastructure;
+using WarehouseService.Interfaces;
+
+namespace WarehouseService.Application.Services;
+
+public class ProductService : IProductService
+{
+    private readonly WarehouseDbContext _dbContext;
+
+    public ProductService(WarehouseDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<ICollection<Product>> GetAsync()
+    {
+        return await _dbContext.Products.AsNoTracking().ToArrayAsync();
+    }
+
+    public async Task CreateAsync(ICollection<Product> products)
+    {
+        await _dbContext.AddRangeAsync(products);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(ICollection<Product> products)
+    {
+        _dbContext.Products.UpdateRange(products);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(ICollection<long> ids)
+    {
+        var toRemove = _dbContext.Products.Where(x=> x.Id.HasValue && ids.Contains(x.Id.Value));
+        _dbContext.Products.RemoveRange(toRemove);
+        await _dbContext.SaveChangesAsync();
+    }
+}
---
+++ Services/WarehouseService/Interfaces/IWarehouseService.cs
@@ -0,0 +1,11 @@
+﻿using WarehouseService.Data.Domain;
+
+namespace WarehouseService.Interfaces;
+
+public interface IWarehouseService
+{
+    Task<ICollection<Warehouse>> GetAsync();
+    Task CreateAsync(ICollection<Warehouse> products);
+    Task UpdateAsync(ICollection<Warehouse> products);
+    Task DeleteAsync(ICollection<long> ids);
+}
