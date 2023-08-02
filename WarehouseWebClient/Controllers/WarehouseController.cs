6ef5164267c3efd58746dcc141cc164b6507be0e
Эдуард Гетман <getman.edik2016@yandex.ru>
2023-08-03 00:40:13 +0300

    Добавлены мастер страница и 2 незаполненые контентом вкладки "Склады" и "Товары" с двумя подвкладками "Товары" и "Остатки".

---
+++ WarehouseWebClient/Controllers/WarehouseController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WarehouseWebClient.Controllers;
+
+public class WarehouseController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Remains()
+    {
+        return View();
+    }
+    public IActionResult Add()
+    {
+        return StatusCode(501);
+    }
+    public IActionResult Update()
+    {
+        return StatusCode(501);
+    }
+    public IActionResult Remove()
+    {
+        return StatusCode(501);
+    }
+}
